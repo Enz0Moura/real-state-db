b2b77ae463e3818720cf28c8ac44a0e5ddf77b43
Enzo Moura <mouraenzo21@gmail.com>
2025-02-03 07:44:40 -0300

    Add multiple tuples test.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <mysql/mysql.h>
 #include "database/settings.h"
+#include "database/tables/Fiador.h"
 #include "database/tables/Inquilino.h"
 
 int main() {
@@ -13,6 +14,15 @@ int main() {
         connection = db.connect();
 
         Inquilino* inquilino = insert_inquilino(connection,"123456", "Kleber", "Maquinista", 100000.32);
+        std::string id_inquilino = inquilino->getAttribute("id");
+
+        Inquilino* inquilino2 = insert_inquilino(connection,"256879", "Joazinho", "Programador", 0.32);
+        std::string id_inquilino2 = inquilino2->getAttribute("id");
+
+        Fiador* fiador2 = insert_fiador(connection, id_inquilino2, "5502", "Jao do biscoito", 99999.32);
+        Fiador* fiador = insert_fiador(connection, id_inquilino, "15879", "Jao Kleber", 1000.32);
+
+
         mysql_close(connection);
     } catch (const std::exception& e){
         std::cerr << "Erro: " << e.what() << std::endl;
