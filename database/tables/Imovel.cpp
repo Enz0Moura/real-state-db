38a1e1d55cb3d3955bdd3ae48c81d2270ede762e
Enzo Moura <mouraenzo21@gmail.com>
2025-02-04 20:17:21 -0300

    Add address fields for Imovel class.

--- database/tables/Imovel.cpp
+++ database/tables/Imovel.cpp
@@ -4,6 +4,26 @@
 
 #include "Imovel.h"
 
+void Imovel::set_cep(std::string cep) {
+    setAttribute("cep",cep);
+}
+
+void Imovel::set_cidade(std::string cidade) {
+    setAttribute("cidade",cidade);
+}
+
+void Imovel::set_bairro(std::string bairro) {
+    setAttribute("bairro",bairro);
+}
+
+void Imovel::set_rua(std::string rua) {
+    setAttribute("rua",rua);
+}
+
+void Imovel::set_complemento(std::string complemento) {
+    setAttribute("complemento",complemento);
+}
+
 void Imovel::set_area_construida(float area_construida) {
     setAttribute("area_construida", std::to_string(area_construida));
 }
@@ -16,11 +36,16 @@ void Imovel::set_num_vagas(int num_vagas) {
     setAttribute("num_vagas", std::to_string(num_vagas));
 }
 
-Imovel* insert_imovel(MYSQL* conn, int num_comodos, int num_vagas, float area_construida) {
+Imovel* insert_imovel(MYSQL* conn, std::string cep, std::string cidade, std::string bairro, std::string rua, std::string complemento, int num_comodos, int num_vagas, float area_construida) {
     auto* imovel = new Imovel();
-    imovel->set_area_construida(area_construida);
+    imovel->set_cep(cep);
+    imovel->set_cidade(cidade);
+    imovel->set_bairro(bairro);
+    imovel->set_rua(rua);
+    imovel->set_complemento(complemento);
     imovel->set_num_comodos(num_comodos);
     imovel->set_num_vagas(num_vagas);
+    imovel->set_area_construida(area_construida);
     imovel->insert(conn);
     return imovel;
 }
--- database/tables/Imovel.h
+++ database/tables/Imovel.h
@@ -12,16 +12,26 @@ class Imovel: public BaseTable {
     Imovel() : BaseTable("imoveis") {
         addColumn("id", UUID);
         setPrimaryKey("id", "UUID");
+        addColumn("cep", "VARCHAR(8)");
+        addColumn("cidade", "VARCHAR(100)");
+        addColumn("bairro", "VARCHAR(100)");
+        addColumn("rua", "VARCHAR(100)");
+        addColumn("complemento", "VARCHAR(100)");
         addColumn("num_comodos", "INTEGER");
         addColumn("num_vagas", "INTEGER");
         addColumn("area_construida", "FLOAT");
     }
+    void set_cep(std::string cep);
+    void set_cidade(std::string cidade);
+    void set_bairro(std::string bairro);
+    void set_rua(std::string rua);
+    void set_complemento(std::string complemento);
     void set_num_comodos(int num_comodos);
     void set_num_vagas(int num_vagas);
     void set_area_construida(float area_construida);
 
 };
 
-Imovel* insert_imovel(MYSQL* conn, int num_comodos, int num_vagas, float area_construida);
+Imovel* insert_imovel(MYSQL* conn, std::string cep, std::string cidade, std::string bairro, std::string rua, std::string complemento, int num_comodos, int num_vagas, float area_construida);
 
 #endif //IMOVEL_H
